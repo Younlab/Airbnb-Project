0e41a11e5f0add5591b5b26ad8ad23dbbaeb2f07
Leesoo <isoo7510@gmail.com>
2018-07-26 20:40:21 +0900

    deploy.sh 작성

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pipenv lock --requirements > requirements.txt
+git add -f .secrets/ requirements.txt
+eb deploy --profile fc-pr-user --staged
+git reset HEAD .secrets/ requirements.txt
+rm requirements.txt
