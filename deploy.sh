a96f9f94b494b8d7295e32589d8c2b19262f18e6
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-30 17:38:42 +0900

    deploy.sh shell script 에 "git add -A" 추가

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 pipenv lock --requirements > requirements.txt
 git add -f .secrets/ requirements.txt
+git add -A
 eb deploy --profile fc-pr-user --staged
 git reset HEAD .secrets/ requirements.txt
 rm requirements.txt
