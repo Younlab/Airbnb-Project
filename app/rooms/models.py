d8b7f639d1b2285dd6518531018332db2f6412fb
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-30 17:57:18 +0900

    rooms/models.py room_info, rule fields => TextField 로 변경

--- app/rooms/models.py
+++ app/rooms/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
 
-# Create your models here.
-
-
 class Room(models.Model):
     Facilities = models.CharField(max_length=400)
     using_info = models.CharField(max_length=200)
     room_info = models.TextField()
-    rule = models.CharField(max_length=200)
+    rule = models.TextField()
     price = models.IntegerField()
     location = models.CharField(max_length=200)
