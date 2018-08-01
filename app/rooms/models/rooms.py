45ef1a802692176375f7d3889183f586424a3371
Sehyeon Youn <d.sehyeon@gmail.com>
2018-08-01 12:22:50 +0900

    rooms models 나누기

--- app/rooms/models/rooms.py
+++ app/rooms/models/rooms.py
@@ -3,6 +3,14 @@ from django.db import models
 from imagekit.models import ImageSpecField
 from pilkit.processors import ResizeToFill
 
+from .facilities import Facilities
+
+__all__ = (
+    'Rooms',
+    'RoomDetail',
+    'RoomImage',
+)
+
 
 class Rooms(models.Model):
     # 호스트가 없으면 숙소 계약이 성립되지 않음, 회원탈퇴하면 글도 지워지도록,
@@ -12,13 +20,16 @@ class Rooms(models.Model):
         related_name='with_host',
     )
 
-    title = models.CharField(max_length=200)
+    # 숙소 이름
+    rooms_name = models.CharField(max_length=50)
+
+    #
     sub_title = models.CharField(max_length=200, blank=True)
 
     tag = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.rooms_name
 
 
 class RoomDetail(models.Model):
@@ -37,13 +48,13 @@ class RoomDetail(models.Model):
         (ROOMS_TYPE_GUESTHOUSE, '게스트하우스'),
     )
 
-    post = models.ForeignKey(
+    # host 정보
+    host = Rooms.host
+
+    rooms = models.ForeignKey(
         Rooms,
         on_delete=models.CASCADE,
     )
-
-    # 숙소 이름
-    rooms_name = models.CharField(max_length=50)
 
     # 숙소 설명
     rooms_description = models.CharField(max_length=200)
@@ -64,10 +75,11 @@ class RoomDetail(models.Model):
     rooms_type = models.CharField(choices=ROOMS_TYPE, max_length=2, default=ROOMS_TYPE_ONEROOM)
 
     # 편의 시설
-    # rooms_facilities = ''
-
-    # 편의 물품
-    # rooms_amenities = ''
+    rooms_facilities = models.ManyToManyField(
+        Facilities,
+        related_name='rooms_facilities',
+        blank=True,
+    )
 
     # 체크인 최소
     check_in_minimum = models.PositiveSmallIntegerField(default=1)
@@ -115,7 +127,7 @@ class RoomDetail(models.Model):
     modified_date = models.DateField(auto_now=True)
 
     def __str__(self):
-        return f'{self.pk} {self.rooms_name}'
+        return f'{self.pk} {self.rooms.rooms_name}'
 
 
 class RoomImage(models.Model):
