3b22e9dce747517a18f2e1c21b87c9b96814fe1f
mingyu <rlaalsrbgk@gmail.com>
2018-08-02 12:20:37 +0900

    signup email 인증 수정 및 인증여부에 따른 로그인 가능 구현

--- app/members/serializers/login.py
+++ app/members/serializers/login.py
@@ -1,22 +1,25 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 
 User = get_user_model()
 
 
-class UserLoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            'username',
-            'password',
-        )
+class UserLoginSerializer(AuthTokenSerializer):
 
     def validate_username(self, value):
         if not User.objects.filter(username=value).exists():
             raise serializers.ValidationError("존재하지 않는 아이디 입니다.")
+
         return value
+
+    def create(self, validated_data):
+        user = User.objects.get(username=validated_data['username'])
+
+        if user.activate is not True:
+            return serializers.ValidationError("인증되지 않은 아이디 입니다.")
+
+        return validated_data
 
 
 
