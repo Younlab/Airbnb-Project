9e3349346a66d99868033f16d2473d58587d5309
Sehyeon Youn <d.sehyeon@gmail.com>
2018-08-03 13:33:10 +0900

    rooms models help text write, old model delete

--- app/rooms/models/rooms.py
+++ app/rooms/models/rooms.py
@@ -3,8 +3,6 @@ from django.db import models
 from imagekit.models import ImageSpecField
 from pilkit.processors import ResizeToFill
 
-from .facilities import Facilities
-
 
 class Rooms(models.Model):
     """
@@ -24,20 +22,28 @@ class Rooms(models.Model):
     )
 
     # 숙소 이름
-    rooms_name = models.CharField(max_length=70)
+    rooms_name = models.CharField(
+        help_text='숙소의 이름을 입력하세요',
+        max_length=70,
+    )
 
     # 태그
-    rooms_tag = models.CharField(max_length=50)
+    rooms_tag = models.CharField(
+        help_text='검색에 사용될 지역 태그를 입력하세요',
+        max_length=50,
+    )
 
     # 호스트
     rooms_host = models.ForeignKey(
         settings.AUTH_USER_MODEL,
+        help_text='숙소의 오너입니다.',
         on_delete=models.CASCADE,
         related_name='with_host_rooms',
     )
 
     # 커버 이미지
     image_cover = models.ImageField(
+        help_text='게스트에게 소개할 숙소의 커버 이미지를 업로드 해 주세요',
         upload_to='room_image_cover',
         blank=True,
     )
@@ -51,41 +57,59 @@ class Rooms(models.Model):
     )
 
     # 일일 요금
-    days_price = models.PositiveIntegerField()
+    days_price = models.PositiveIntegerField(
+        help_text='일일 숙박 요금을 입력하세요'
+    )
 
     # 숙소 설명
-    rooms_description = models.CharField(max_length=200)
+    rooms_description = models.CharField(
+        help_text='당신의 숙소를 소개하세요, 게스트의 흥미를 유발하는것이 중요합니다.',
+        max_length=200
+    )
 
     # 객실 수
-    rooms_amount = models.PositiveSmallIntegerField()
+    rooms_amount = models.PositiveSmallIntegerField(
+        help_text='숙소 내의 객실 수를 입력하세요'
+    )
 
     # 침대 갯수
-    rooms_bed = models.PositiveSmallIntegerField()
+    rooms_bed = models.PositiveSmallIntegerField(
+        help_text='객실 내의 침대 수를 입력하세요'
+    )
 
     # 숙박 인원
-    rooms_personnel = models.PositiveSmallIntegerField()
+    rooms_personnel = models.PositiveSmallIntegerField(
+        help_text='최대 숙박 가능인원을 입력하세요'
+    )
 
     # 욕실 갯수
-    rooms_bathroom = models.PositiveSmallIntegerField()
+    rooms_bathroom = models.PositiveSmallIntegerField(
+        help_text='숙소 내의 욕실 수를 입력하세요'
+    )
 
     # 숙소 유형
-    rooms_type = models.CharField(choices=ROOMS_TYPE, max_length=2, default=ROOMS_TYPE_ONEROOM)
-
-    # 편의 시설
-    rooms_facilities = models.ManyToManyField(
-        Facilities,
-        related_name='rooms_facilities',
-        blank=True,
+    rooms_type = models.CharField(
+        help_text='숙소의 유형을 선택해주세요',
+        choices=ROOMS_TYPE,
+        max_length=2, default=ROOMS_TYPE_ONEROOM
     )
 
     # 체크인 최소
-    check_in_minimum = models.PositiveSmallIntegerField(default=1)
+    check_in_minimum = models.PositiveSmallIntegerField(
+        help_text='최소 숙박 가능일 수를 입력해주세요',
+        default=1,
+    )
 
     # 체크인 최대
-    check_in_maximum = models.PositiveSmallIntegerField(default=3)
+    check_in_maximum = models.PositiveSmallIntegerField(
+        help_text='최대 숙박 가능일 수를 입력해주세요',
+        default=3,
+    )
 
     # 환불규정
-    refund = models.TextField()
+    refund = models.TextField(
+        help_text='환불 규정을 가급적 상세히 입력해주세요'
+    )
 
     # 나라
     address_country = models.CharField(max_length=100)
@@ -148,3 +172,22 @@ class RoomImgeList(models.Model):
     room_image_list = models.ImageField(
         upload_to='room_image_list'
     )
+
+
+class RoomFacilities(models.Model):
+    """
+    편의시설 리스트
+    """
+    room = models.ForeignKey(
+        Rooms,
+        on_delete=models.CASCADE,
+        related_name='room_facilities'
+    )
+
+    facilities = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+
+    def __str__(self):
+        return f'{self.facilities}'
