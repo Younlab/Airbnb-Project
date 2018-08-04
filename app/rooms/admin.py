dc5467a632e4846d7d53348b8215c969a57ee1c4
Sehyeon Youn <d.sehyeon@gmail.com>
2018-08-04 23:51:15 +0900

    rooms/admin.py RoomImageList, RoomFacilities add

--- app/rooms/admin.py
+++ app/rooms/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Rooms
+from .models import Rooms, RoomFacilities, RoomImageList
 
 admin.site.register(Rooms)
+admin.site.register(RoomFacilities)
+admin.site.register(RoomImageList)
