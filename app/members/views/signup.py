6d1f2d0d37253382d018780d5cddef5b54c4defc
mingyu <rlaalsrbgk@gmail.com>
2018-08-07 12:21:56 +0900

    signup refact

--- app/members/views/signup.py
+++ app/members/views/signup.py
@@ -22,7 +22,7 @@ class UserSignup(APIView):
         serializer = UserSignupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
