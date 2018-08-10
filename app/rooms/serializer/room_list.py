ade3dbbcc80544400588fb14077d327f3ba704a4
Sehyeon Youn <d.sehyeon@gmail.com>
2018-08-10 10:38:44 +0900

    serializer.room_list.py RoomReservationSerializer,RoomDetailSerializer,RoomRuleSerializer,RoomFacilitieSerializer,RoomReservationSerializer 정의

--- app/rooms/serializer/room_list.py
+++ app/rooms/serializer/room_list.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from members.serializers.user import UserSerializer
-from ..models.rooms import RoomReservation
+from ..models.rooms import RoomReservation, RoomFacilities, RoomRules
 from ..models import Rooms
 
 
@@ -18,17 +18,77 @@ class RoomListSerializer(serializers.ModelSerializer):
         )
 
 
-class RoomDetailSerializer(serializers.ModelSerializer):
-    rooms_host = UserSerializer()
+class RoomReservationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Rooms
+        model = RoomReservation
         fields = '__all__'
 
 
+class RoomFacilitieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoomFacilities
+        fields = (
+            'facilities',
+        )
+
+
+class RoomRuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoomRules
+        fields = (
+            'rule_list',
+        )
+
+
+class RoomDetailSerializer(serializers.ModelSerializer):
+    rooms_host = UserSerializer()
+    room_facilities = RoomFacilitieSerializer(many=True)
+    room_reservations = RoomReservationSerializer(many=True)
+    room_rules = RoomRuleSerializer(many=True)
+
+    class Meta:
+        model = Rooms
+        fields = (
+            'rooms_name',
+            'rooms_tag',
+            'rooms_host',
+            'rooms_type',
+            'image_cover',
+            'rooms_amount',
+            'rooms_bed',
+            'rooms_personnel',
+            'rooms_bathroom',
+            'days_price',
+            'room_rules',
+            'room_facilities',
+            'rooms_description',
+            'check_in_minimum',
+            'check_in_maximum',
+            'room_reservations',
+            'refund',
+            'address_country',
+            'address_city',
+            'address_district',
+            'address_detail',
+            'address_latitude',
+            'address_longitude',
+            'created_at',
+            'modified_date',
+
+        )
+
+
 class RoomReservationSerializer(serializers.ModelSerializer):
+    guest = UserSerializer()
     checkin = serializers.DateField()
     checkout = serializers.DateField()
 
     class Meta:
         model = RoomReservation
-        fields = '__all__'
+        fields = (
+            'room',
+            'guest',
+            'checkin',
+            'checkout',
+            'created_at',
+        )
