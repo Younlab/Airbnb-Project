96fddc0a0449008d2a9d3f5778c22363d8f8de53
Leesoo <isoo7510@gmail.com>
2018-08-10 18:43:53 +0900

    test_not_admin_status_code_403 완성

--- app/members/tests.py
+++ app/members/tests.py
@@ -4,6 +4,14 @@ from rest_framework.test import APITestCase
 
 User = get_user_model()
 
+user = {
+    'username': 'dummy_username@test.com',
+    'first_name': 'user',
+    'last_name': 'test',
+    'birthday': '000000',
+    'password': 'test1234'
+}
+
 
 class UserListTest(APITestCase):
     """
@@ -11,11 +19,19 @@ class UserListTest(APITestCase):
     """
     URL = '/members/userlist/'
 
-    def test_user_list_status_code(self):
+    def test_not_admin_status_code_403(self):
         """
-        요청 결과의 HTTP 상태코드가 400인지 확인
+        superuser가 아닌 일반 유저일 경우에 요청 결과의 HTTP 상태코드가 403인지 확인
         :return:
         """
+        # user 회원가입 시키기 & 이메일 인증 True로 바꾸고 저장
+        login_user = User.objects.create_django_user(**user)
+        login_user.activate = True
+        login_user.save()
+
+        # 로그인을 그냥 바로 시켜버림!
+        self.client.force_authenticate(login_user)
 
         response = self.client.get(self.URL)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        print(response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
