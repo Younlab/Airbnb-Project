cfde569041c4a6bf8ec9fb0242b6e9381b38fa73
Leesoo <isoo7510@gmail.com>
2018-08-10 18:43:18 +0900

    setdb.sh 끝

--- setdb.sh
+++ setdb.sh
@@ -8,11 +8,15 @@ echo "START ./setdb.sh"
 
 HOST='myairbnbproject.ccrdezcnkmuh.ap-northeast-2.rds.amazonaws.com'
 USER='leesoo'
-PORT='5432'
+PORT=5432
+#PASSWORD='michael6295'
 DB='postgres'
-DROP_DB='DROP DATABASE airbnb_rds;'
-CREATE_DB='CREATE DATABASE airbnb_rds OWNER leesoo;'
 
-PGPASSWORD='michael6295' psql --host=${HOST} --user=${USER} --port=${PORT} ${DB} -c ${DROP_DB}
+#DROP_DB='DROP DATABASE airbnb_rds;'
+#CREATE_DB='CREATE DATABASE airbnb_rds OWNER leesoo;'
 
-PGPASSWORD='michael6295' psql --host=${HOST} --user=${USER} --port=${PORT} ${DB} -c ${CREATE_DB}
+#PGPASSWORD='michael6295' psql --host=${HOST} --user=${USER} --port=${PORT} ${DB} -c ${DROP_DB}
+
+#PGPASSWORD='michael6295' psql --host=${HOST} --user=${USER} --port=${PORT} ${DB} -c ${CREATE_DB}
+PGPASSWORD='michael6295' psql --host=${HOST} --user=${USER} --port=${PORT} ${DB} -c 'DROP DATABASE airbnb_rds;'
+PGPASSWORD='michael6295' psql --host=${HOST} --user=${USER} --port=${PORT} ${DB} -c 'CREATE DATABASE airbnb_rds OWNER leesoo;'
