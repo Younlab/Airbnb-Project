08c3677ea0b33e96f0920b2d4abd43d552aa2de2
Leesoo <isoo7510@gmail.com>
2018-08-10 17:53:52 +0900

    user_list 제네릭뷰로 수정

--- app/members/views/user_list.py
+++ app/members/views/user_list.py
@@ -1,20 +1,14 @@
 from django.contrib.auth import get_user_model
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
+from ..permissions import AdminUserReadOnly
 from ..serializers.user import UserSerializer
 
 User = get_user_model()
 
 
-class UserList(APIView):
+class UserList(generics.ListAPIView):
 
-    def get(self, request):
-
-        if request.user.is_superuser:
-            user_list = User.objects.all()
-            serializer = UserSerializer(user_list, many=True)
-
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response('볼수있는 권한이 없습니다.', status=status.HTTP_400_BAD_REQUEST)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AdminUserReadOnly,)
