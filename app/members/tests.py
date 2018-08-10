a62a6415cf97ec4b94e933f42964fcd61363ef41
Leesoo <isoo7510@gmail.com>
2018-08-10 17:55:10 +0900

    test dummyuser 생성 구현중

--- app/members/tests.py
+++ app/members/tests.py
@@ -5,6 +5,18 @@ from rest_framework.test import APITestCase
 User = get_user_model()
 
 
+def get_dummy_user():
+    user = User.objects.create_user(
+        username='dummy_username@test.com',
+        first_name='user',
+        last_name='test',
+        birthday='000000',
+        password='test1234',
+    )
+    user.activate = True
+    return user
+
+
 class UserListTest(APITestCase):
     """
     User List 요청에 관한 테스트
@@ -13,9 +25,10 @@ class UserListTest(APITestCase):
 
     def test_user_list_status_code(self):
         """
-        요청 결과의 HTTP 상태코드가 400인지 확인
+        superuser가 아닌 일반 유저일 경우에 요청 결과의 HTTP 상태코드가 403인지 확인
         :return:
         """
-
+        login_user = get_dummy_user()
+        print(login_user)
         response = self.client.get(self.URL)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
