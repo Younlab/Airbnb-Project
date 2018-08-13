436d0bee078001c9db5dc9f0d3ac4f0f1f903cd7
Leesoo <isoo7510@gmail.com>
2018-08-13 13:19:50 +0900

    test.py email token값 비교

--- app/members/tests.py
+++ app/members/tests.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import get_user_model
+from django.urls import path
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from members.tokens import account_activation_token
+
 User = get_user_model()
 
-user = {
+dummy_user = {
     'username': 'dummy_username@test.com',
     'first_name': 'user',
     'last_name': 'test',
@@ -25,12 +28,12 @@ class UserListTest(APITestCase):
         :return:
         """
         # user 회원가입 시키기 & 이메일 인증 True로 바꾸고 저장
-        login_user = User.objects.create_django_user(**user)
-        login_user.activate = True
-        login_user.save()
+        user = User.objects.create_django_user(**dummy_user)
+        user.activate = True
+        user.save()
 
         # 로그인을 그냥 바로 시켜버림!
-        self.client.force_authenticate(login_user)
+        self.client.force_authenticate(user)
 
         response = self.client.get(self.URL)
         print(response.status_code)
@@ -43,12 +46,34 @@ class UserSignUpTest(APITestCase):
     """
     URL = '/members/signup/'
 
-    def test_user_sign_up_succeed_status_code(self):
+    def test_user_sign_up_succeed_status_code_201(self):
         """
         user 회원가입 정보가 정상적으로 요청되었을 때 요청 결과의 HTTP 상태코드가 201인지 확인
         json으로 요청하고 201 상태코드를 받아옴
         :return:
         """
 
-        response = self.client.post(self.URL, data=user, format='json',)
+        response = self.client.post(self.URL, data=dummy_user, format='json', )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class UserEmailActivateCheckTest(APITestCase):
+    """
+    User SignUp 시에 Email 인증 관련 테스트
+    """
+    URL = '/members/activate/<str:uidb64>/<str:token>/'
+
+    def test_check_user_email_token_equal(self):
+        """
+        user 회원가입 후 발송되는 인증 확인 이메일 token값과 DB에 저장되는 이메일 token 값이 같은지 확인
+        :return:
+        """
+        # user 회원가입 시키기 & 이메일 인증 True로 바꾸고 저장
+        user = User.objects.create_django_user(**dummy_user)
+        user.activate = True
+        user.save()
+
+        # user email token 값과 DB상의 토큰 값 비교
+        token = account_activation_token.make_token(user)
+        self.assertTrue(account_activation_token.check_token(user, token))
+
