1491095d0be681b77f0882317a185f37fe60a578
Sehyeon Youn <d.sehyeon@gmail.com>
2018-08-15 20:49:28 +0900

    main list, main detail, main reservation url add

--- app/rooms/urls.py
+++ app/rooms/urls.py
@@ -5,9 +5,14 @@ from .views import room_list, room_like
 app_name = 'rooms'
 
 urlpatterns = [
+    # 메인 room list
+    path('main/', room_list.MainPageRoomsList.as_view(), name='main_list'),
+    path('main/<int:room_pk>/', room_list.RoomsDetail.as_view(), name='main_detail'),
+    path('main/<int:room_pk>/reservation/', room_list.RoomReservationAPI.as_view(), name='main_reservation'),
+
+    # 기본 room list
     path('list/', room_list.RoomsList.as_view(), name='room_list'),
     path('list/<int:room_pk>/', room_list.RoomsDetail.as_view(), name='room_detail'),
     path('list/<int:room_pk>/reservation/', room_list.RoomReservationAPI.as_view(), name='room_reservation'),
-    path('main/snipetlist/', room_list.MainPageRoomsList.as_view(), ),
     path('list/<int:pk>/likes/', room_like.RoomsLikes.as_view(), name='room_like'),
 ]
