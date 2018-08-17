2343e7727f988931b3c3664b344db55f72f5e7b8
Leesoo <isoo7510@gmail.com>
2018-08-17 18:35:01 +0900

    modify generic view 변경 주석

--- app/members/views/modify.py
+++ app/members/views/modify.py
@@ -10,7 +10,9 @@ User = get_user_model()
 class UserProfileModified(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserProfileSerializer
+    # permission.py IsAuthenticated로 request.user.is_authenticated 확인
     permission_classes = (IsAuthenticated,)
 
+    # default로 pk를 가져오는 부분 customizing
     def get_object(self):
         return self.request.user
