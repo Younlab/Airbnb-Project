6e0c754ccc0f691146eb59d9c9cbc3e48c8b0fad
Leesoo <isoo7510@gmail.com>
2018-08-17 11:19:55 +0900

    test patch user profile status code 201

--- app/members/tests.py
+++ app/members/tests.py
@@ -215,3 +215,13 @@ class UserProfileTest(APITestCase):
 
         response = self.client.get(self.URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_patch_user_profile_status_code_201(self):
+        user = get_dummy_user()
+        self.client.force_authenticate(user)
+
+        patch_user = {
+            'birthday': '000001'
+        }
+        response = self.client.patch(self.URL, data=patch_user)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
