f5094af055f9ac0d8056a4717a58a5ddc2c81755
Leesoo <isoo7510@gmail.com>
2018-08-20 01:18:10 +0900

    test rooms 기본 dummy user

--- app/rooms/tests.py
+++ app/rooms/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+User = get_user_model()
+
+DUMMY_USER_INFO = {
+        'username': 'dummy_username@test.com',
+        'first_name': 'user',
+        'last_name': 'test',
+        'birthday': '000000',
+        'password': 'test1234',
+    }
+
+LOGIN_USER = {
+        'username': DUMMY_USER_INFO['username'],
+        'password': DUMMY_USER_INFO['password'],
+    }
+
+
+# user 회원가입 시키기 & 이메일 인증 True로 바꾸고 저장
+def get_dummy_user():
+    try:
+        user = User.objects.get(**DUMMY_USER_INFO)
+        return user
+    except User.DoesNotExist:
+        user = User.objects.create_django_user(**DUMMY_USER_INFO)
+        user.activate = True
+        user.save()
+        return user
+
